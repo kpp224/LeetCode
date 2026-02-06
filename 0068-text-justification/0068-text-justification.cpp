dafdb03c766c053756a23cdeddf3be135a5d3763
Kush Patel <juug24lbtech12928@jainuniversity.ac.in>
2026-02-06 12:27:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (39.21%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> ans;
+        int i = 0;
+
+        while(i < words.size()){
+            int j = i;
+            int letters = 0;
+
+            while(j < words.size() && letters + words[j].size() + (j - i) <= maxWidth){
+                letters += words[j].size();
+                j++;
+            }
+
+            int gap = j - i - 1;
+            int space = maxWidth - letters;
+
+            string line = "";
+
+            if(j == words.size() || gap == 0){
+                for(int k = i; k < j; k++){
+                    line += words[k];
+                    if(k < j - 1){
+                        line += " ";
+                    }
+                }
+
+                while(line.size() < maxWidth){
+                    line += " ";
+                }
+            }
+            else{
+                int eachSpace = space / gap;
+                int extraSpace = space % gap;
+
+                for(int k = i; k < j; k++){
+                    line += words[k];
+
+                    if(k < j - 1){
+                        int s = eachSpace;
+                        if(extraSpace > 0){
+                            s++;
+                            extraSpace--;
+                        }
+
+                        line += string(s, ' ');
+                    }
+                }
+            }
+
+            ans.push_back(line);
+            i = j;
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+
