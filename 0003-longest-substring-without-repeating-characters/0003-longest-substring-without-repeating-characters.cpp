62ac4e79758d143e906fd32e119bc4cfb8440831
Kush Patel <juug24lbtech12928@jainuniversity.ac.in>
2026-02-23 12:49:45 +0530

    Time: 20 ms (22.87%), Space: 14.4 MB (21.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> st;
-        int n = s.length();
 
         int left = 0;
         int maxLen = 0;
 
-        for(int right = 0; right < n; right++){
+        for(int right = 0; right < s.length(); right++){
             while(st.count(s[right])){
                 st.erase(s[left]);
                 left++;
@@ -18,6 +17,5 @@ public:
         }
 
         return maxLen;
-
     }
 };
