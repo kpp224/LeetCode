eeb0a37d45f9572c8463a90e1cc7cb25d16d0d1e
Kush Patel <juug24lbtech12928@jainuniversity.ac.in>
2026-02-26 12:43:25 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.1%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool firstRowMarker = false;
+        bool firstColMarker = false;
+
+        for(int j = 0; j < n; j++){
+            if(matrix[0][j] == 0)
+            firstRowMarker = true;
+        }
+
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] == 0)
+            firstColMarker = true;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowMarker){
+            for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColMarker){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
