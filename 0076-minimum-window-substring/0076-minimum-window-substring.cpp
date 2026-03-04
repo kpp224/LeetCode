a2a5466f0b9ade258349c8404081044cf444ada5
Kush Patel <kushpp224@gmail.com>
2026-03-04 12:08:55 +0530

    Time: 18 ms (24.91%), Space: 11.6 MB (43.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,46 +3,43 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char,int> need;
 
-        for(char c : t)
-        need[c]++;
+        for(char c : t){
+            need[c]++;
+        }
 
         int left = 0;
         int minLen = INT_MAX;
         int start = 0;
-        int formed = 0;
-        int required = need.size();
-        
+        int formed = need.size();
+        int required = 0;
+
         unordered_map<char,int> window;
 
         for(int right = 0; right < s.length(); right++){
             char c = s[right];
             window[c]++;
 
-            if(need.count(c) && need[c] == window[c])
-            formed++;
+            if(need[c] && need[c] == window[c])
+            required++;
 
-            while(formed == required){
+            while(required == formed){
                 if(right - left + 1 < minLen){
                     minLen = right - left + 1;
                     start = left;
                 }
 
-                char leftChar = s[left];
+                int leftChar = s[left];
                 window[leftChar]--;
 
-                if(need.count(leftChar) && window[leftChar] < need[leftChar]){
-                    formed--;
-                }
+                if(need[leftChar] && window[leftChar] < need[leftChar])
+                required--;
 
                 left++;
             }
         }
 
-        if(minLen == INT_MAX){
-            return "";
-        }
+        if(minLen == INT_MAX) return "";
 
         return s.substr(start,minLen);
-
     }
 };
