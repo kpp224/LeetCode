e805ae91d20863855e871012f7f32f7319a3dd16
Kush Patel <kushpp224@gmail.com>
2026-06-02 15:37:55 +0530

    Time: 0 ms (100%), Space: 77.2 MB (43.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -20,12 +20,12 @@ public:
             int sum = 0 + carry;
 
             if(l1 != NULL){
-                sum = sum + l1->val;
+                sum += l1->val;
                 l1 = l1->next;
             }
 
             if(l2 != NULL){
-                sum = sum + l2->val;
+                sum += l2->val;
                 l2 = l2->next;
             }
 
