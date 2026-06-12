1a5dcdd88a84defac1152309628cf4dfee2eb6d4
Kush Patel <kushpp224@gmail.com>
2026-06-12 12:17:45 +0530

    Time: 51 ms (30.87%), Space: 56.2 MB (56.58%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    var chunkArray = [];
+    var index = 0;
+
+    while(index < arr.length){
+        chunkArray.push(arr.slice(index, index + size));
+        index += size;
+    }
+
+    return chunkArray;
+};
